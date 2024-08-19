fef9d1b2daced298489e39013cbabbb58478dfd2
Feraidoon Hamidi <feraidoon321@hotmail.com>
2024-08-19 02:17:30 +0200

    feat: Add pydantic schema for admissions

---
+++ app/schemas/admission.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from pydantic import Field
+
+class AdmissionCreate(BaseModel):
+    patient_id: int = Field(..., gt=0, description="ID of patient to be admitted.")
+    department_id: int = Field(..., gt=0, description="ID of Department assigned to this admission.")
+    doctor_ids: list[int] | None = Field(default=[], description="List of doctor(s) ID(s) assigned to this admission")
+
+    class ConfigDict:
+        json_schema_extra = {
+            "example": {
+                "patient_id": 1,
+                "department_id": 4,
+                "doctor_ids": [1, 2]
+            }
+        }
+
+class Admission(AdmissionCreate):
+    id: int
+
+    class ConfigDict:
+        from_attributes = True
